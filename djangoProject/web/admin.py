af097bd1636e4bc70961a0b502421912c8c0fa7a
proo13provo <hacaotantruong0@gmail.com>
2024-12-14 20:51:23 +0700

    update cart

--- djangoProject/web/admin.py
+++ djangoProject/web/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Prodcut)
+admin.site.register(Product)
 admin.site.register(Oder)
 admin.site.register(Oder_Iterm)
 
