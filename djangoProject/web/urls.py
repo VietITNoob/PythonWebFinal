3caac0c6db7bdee33a2721761369db8450f220f8
proo13provo <hacaotantruong0@gmail.com>
2024-12-17 13:46:38 +0700

    update index Ui

--- djangoProject/web/urls.py
+++ djangoProject/web/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('search.html/', views.search, name='search'),
     path('productDetail.html/', views.productDetails, name='productDetail'),
     path('cart.html/', views.view_cart, name='view_cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update_quantity/', views.update_quantity, name='update_quantity'),
 ]
 
